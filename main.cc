711c4b8d613fb9918ac1ac9bffb39e32384b0dc4
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-01 21:31:13 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <unorderedmap>
+#include <unordered_map>
 #include "hash.h"
+
+
+
 
 
 using namespace std; 
 
 int main (){
-
-
+unordered_map<login,int> database(100000);
 cout << "Welcome to the the card Game" << endl; 
 cout << "Are you a new user? (Y/N)" << endl;
       char c;
       cin >> c;
       if (c == y || c == Y){
-      login new_user;      
-      
+      login new_user;
+      new_user.set_username();
+      new_user.set_password();
+      }
+      else {
+      // look up username and login from database.
       
       }
-      else {}
 
 
 
