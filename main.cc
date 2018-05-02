0d3472f2d4074e5e0c914c76831d9da106cdd1c7
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-01 21:54:31 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,9 +15,16 @@ cout << "Are you a new user? (Y/N)" << endl;
       char c;
       cin >> c;
       if (c == y || c == Y){
-      login new_user;
-      new_user.set_username();
-      new_user.set_password();
+            login new_user;
+            string username;
+            string password;
+            cout << "Enter new username" << endl;      
+            cin >> username;      
+            cout << "Enter new password" << endl;
+            cin >> password;
+            new_user.set_username(username);
+            new_user.set_password(password);
+            database      
       }
       else {
       // look up username and login from database.
