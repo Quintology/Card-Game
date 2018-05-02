f6e1398f549c430c6505b1b98b82c1e5de954c18
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-02 11:02:00 -0700

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include "login.h"
+#include "login.cc"
+
+using namespace std; 
+
+void die(){
+      cout << "Invalid Input" << endl;
+      exit(1);
+}
+
+
+int main(){
+
+login_hash();
+
+
+
+return 0;
+
+}
