47597bfb0ac98ac720be67a1af18e2d75e2a8c00
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-01 21:57:21 -0700

    Update login.h

--- login.h
+++ login.h
@@ -5,7 +5,7 @@ struct Login {
     std::string username;
     std::string password;
     long long deck = 0;
-    bool operator< (const Competitor &rhs) const {
-        return ID > rhs.ID;
+    bool operator< (const Login &rhs) const {
+        return deck > rhs.deck;
     }
 };
