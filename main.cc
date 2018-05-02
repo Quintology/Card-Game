41be7f88e72e369b8096c2427ed09176be453bd9
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-01 21:05:43 -0700

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unorderedmap>
+#include "hash.h"
+
+
+using namespace std; 
+
+int main (){
+
+
+cout << "Welcome to the the card Game" << endl; 
+cout << "Are you a new user? (Y/N)" << endl;
+      char c;
+      cin >> c;
+      if (c == y || c == Y){
+      login new_user;      
+      
+      
+      }
+      else {}
+
+
+
+
+
+
+
+
+
+}
