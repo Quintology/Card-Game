fe4d22fd76167cba8a6aa5f94c1b5fa20656882f
Alfredo Gonzalez <32611241+alfredomania@users.noreply.github.com>
2018-05-03 21:23:35 -0700

    Add files via upload
    rough details on what the battle phases will look like :/

---
+++ battle.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+int main(){
+
+//User will inpute login info, holding a vector with numbers.
+//The list of numbers will be used with a bitfield holding the cards info.
+
+char a;
+int energy;
+Cardfield cards;
+string card_name;
+for(int i=0; i<3;i++){
+	//pops of the first 3 cards(each user begins with 3 cards.
+}
+
+
+
+
+while(cin){
+cout<<"player 1's turn"<<endl;
+//Need a stack or something to pop the cards off the deck lol.
+
+//draw card
+
+
+//phase 1: user draws a card from hand
+
+//phase 2: user places card from hand to field
+cin>>card;
+if(card_name > energy){
+cout<<"Not enough energy"<<endl;
+}
+if(card_name< energy){
+//user is able to place down card. pop of hand card to field.
+}
+
+//phase 3: user goes into attack phase
+cout<<"choose a card to attack with"<<endl;
+cin>>card_name;
+
+cout<<"choose which what you want to attack. Enter card name or Player"<<endl;
+cin<<card_name;
+
+//dont know full details on this yet, looks through enemy card field
+for(int i=0; i<Enemy_field.size; i++){
+if(card_name == Enemy_field.at(i)){
+//player 1 card will compare to player 2 card
+//player 1 card damage will subtract player 2 card health vice versa 
+
+//if player 1 card or player 2 card looses less than 1 health, card gets destroyed
+
+}
+if(card_name == "player"){
+//this is if user decides to attack the player directly.
+	//something like
+	player2 = player2 - card_damage;
+}
+
+		}
+
+}
+
+
+}
+
+
+
