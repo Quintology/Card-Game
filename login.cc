ade5596d51c91cd50fd0079ffc92976f102b2c77
jacob33c <38203225+jacob33c@users.noreply.github.com>
2018-05-04 00:28:58 -0700

    Create Login.cc
    this will write into a file. sort the file into a hashtable

---
+++ login.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+
+using namespace std;
+void writeFile(string s) {
+    ofstream myfile;
+    myfile.open("login.txt");
+    cout << "Write into the file" << endl;
+    cin >> s;
+    myfile << s << endl;
+    myfile.close();
+}
+struct Login {
+    std::string username;
+    std::string password;
+    long long deck = 0;
+    bool operator< (const Login &rhs) const {
+        return deck > rhs.deck;
+    }
+};
+
+
+
+void file_into_hash() {
+
+    unordered_map<int, Login > database;
+    ofstream myfile;
+    myfile.open("login.txt");
+    myfile.close();
+}
+
+
+
+
+
+void login_hash() {
+    cout << "Welcome to the the card Game" << endl;
+    cout << "Are you a new user? (Y/N)" << endl;
+    char c;
+    cin >> c;
+    // if yes create new user
+    if (c == 'y' || c == 'Y') {
+        string username;
+        string password;
+        cout << "Enter new username" << endl;
+        cin >> username;
+        cout << "Enter new password" << endl;
+        cin >> password;
+        if (database.find(c.username)) {
+            die();
+        }
+        writeFile(username);
+        writeFile(password);
+    }
+    // if not look up username and password in database;
+    else {
+        // look up username and login from database.
+        string username;
+        string password;
+        cout << "Enter your username" << endl;
+        cin >> username;
+        cout << "Enter your password" << endl;
+        cin >> password;
+        if (database.find(c.username)) {
+            if (database[c.username] == database[c.password]) {
+                cout << "You are now logged in" << endl;
+            }
+        } else die();
+
+    }
+
+
+
+
+}
+int main() {
+
+    login_hash();
+
+
+
+}
+
