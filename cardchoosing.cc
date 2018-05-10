b292eb4e697ad5a028259a597b8125121879b8b0
Christopher Quinto <38361476+Quintology@users.noreply.github.com>
2018-05-10 00:36:25 -0700

    Update cardchoosing.cc

--- cardchoosing.cc
+++ cardchoosing.cc
@@ -3,46 +3,44 @@
 using namespace std;
 
 int damage(string card) {
-
-	if (card == "goblin")return 3;
-
+    if (card == "goblin") return 3;
+    else if (card == "veteran_goblin") return 6; //might try to use getlines so people don't need to type in an _ to summon a card
+    else cout << "Why don't you try again." << endl;
+//  exit(1);
 }
 
 int health(string card) {
-	if (card == "goblin")return 2;
+    if (card == "goblin")return 2;
+    else if (card == "veteran_goblin") return 4;
+    else cout << "You really have chosen nothing." << endl;
+    exit(1);
 }
 
 
 int main() {
 
-	string name;
+    string name;
+    bool f1 = false;
+    string field1;
+    int field1_damage;
+    int field1_health;
 
-	bool f1 = false;
+    cout << "Enter card name to place on field" << endl;
 
-	string field1;
-	int field1_damage;
-	int field1_health;
+    cin >> name;
 
-	cout << "Enter card name to place on field" << endl;
-
-	cin >> name;
-
-	if (f1 == false) {
-		field1 = name;
-		field1_damage = health(name);
-		field1_health = damage(name);
-	}
-
-
-	cout << field1 << endl;
-	cout << field1_damage << endl;
-	cout << field1_health << endl;
-
-	return 0;
+    if (f1 == false) {
+        field1 = name;
+//      field1_damage = health(name); (original code)
+//      field1_health = damage(name);
+        field1_damage = damage(name);  //(had to swap these around)
+        field1_health = health(name);  //(it was displaying the damage as health and the health as damage)
+    }
 
 
+    cout << "You have chosen: " << field1 << endl;
+    cout << field1 << " Damage: " << field1_damage << endl;
+    cout << field1 << " Health: " << field1_health << endl;
 
-
-
-
+    return 0;
 }
