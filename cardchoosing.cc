a6add1ca59d555243ffffbfadcdd70d6d3642c4f
Christopher Quinto <38361476+Quintology@users.noreply.github.com>
2018-05-10 18:02:59 -0700

    Update cardchoosing.cc

--- cardchoosing.cc
+++ cardchoosing.cc
@@ -2,19 +2,24 @@
 #include <string>
 using namespace std;
 
+//damage function
 int damage(string card) {
-    if (card == "goblin") return 3;
-    else if (card == "veteran_goblin") return 6; //might try to use getlines so people don't need to type in an _ to summon a card
-    else if (card == "mountain_giant") return 6;
-    else if (card == "midget_giant") return 5;
-    else if (card == "midget_army")  return 8;
-    else if (card == "army") return 10;
-    else if (card == "army_of_the_dead") return 12;
-    else if (card == "dragon")  return 15;
+    if (card == "goblin") return 1;
+    else if (card == "veteran_goblin") return 2; //might try to use getlines so people don't need to type in an _ to summon a card
+    else if (card == "mountain_giant") return 2;
+    else if (card == "midget_giant") return 1;
+    else if (card == "midget_army")  return 3;
+    else if (card == "army") return 6;
+    else if (card == "army_of_the_dead") return 7;
+    else if (card == "dragon")  return 8;
+    else if (card == "balrog") return 5;
+    else if (card == "demon_frog") return 1;
+    else if (card == "devil_duck") return 2;
+    else if (card == "might_manatee") return 4;
 }
-
+//health function
 int health(string card) {
-     if (card == "list") {
+    if (card == "list") {
         cout << "goblin" << endl;
         cout << "veteran_goblin" << endl;
         cout << "mountain_giant" << endl;
@@ -23,19 +28,30 @@ int health(string card) {
         cout << "army" << endl;
         cout << "army_of_the_dead" << endl;
         cout << "dragon" << endl;
+        cout << "balrog" << endl;
+        cout << "demon_frog" << endl;
+        cout << "devil_duck" << endl;
+        cout << "might_manatee" << endl;
     } else if (card == "goblin")return 2;
-    else if (card == "veteran_goblin") return 3;
+    else if (card == "veteran_goblin") return 4;
     else if (card == "mountain_giant") return 5;
-    else if (card == "midget_giant") return 4;
-    else if (card == "midget_army")  return 7;
-    else if (card == "army") return 12;
-    else if (card == "army_of_the_dead") return 15;
-    else if (card == "dragon")  return 20;
+    else if (card == "midget_giant") return 6;
+    else if (card == "midget_army")  return 5;
+    else if (card == "army") return 7;
+    else if (card == "army_of_the_dead") return 8;
+    else if (card == "dragon")  return 10;
+    else if (card == "balrog") return 3;
+    else if (card == "demon_frog") return 1;
+    else if (card == "devil_duck") return 2;
+    else if (card == "might_manatee") return 4;
     else
         cout << "You really have chosen nothing. Why don't you try again." << endl;
     exit(1);
 }
 
+//cost function
+int cost(string card) {
+}
 
 //alfredo doing dis main
 int main() {
